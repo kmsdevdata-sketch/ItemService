c3450b69ce8fa454c49a60b3003c3c63a6945b77
kmsdevdata-sketch <kmsdevdata@gmail.com>
2026-01-05 17:25:57 +0900

    RedirectAttribute

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -88,11 +89,29 @@ public class BasicItemController {
 //        return "basic/item";
 //    }
     //심지어 @ModelAttribute도 생략이 가능함
-    @PostMapping("/add")
+    /* 문제점
+    * 현재 문제는 사용자가 새로고침시에 마지막으로 보냈던 요청인 Post요청을 다시날려서
+    * 상품이 계속 등록되는 문제가 생김
+    * 해결하기위해서 PRG(Post/Redirect/Get)을 도입하여
+    * 뷰반환이 아닌 리다이렉트 요청을 하여 마지막요청을 Post가 아닌 새로운페이지 조회로 변경하여
+    * 새로고침을 눌러도 문제가 발생하지 않게 한다
+     * */
+//    @PostMapping("/add")
     public String addItemV4(Item item) {
-
         itemRepository.save(item);
         return "basic/item";
+    }
+//    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
+    }
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
